9bee0b37934f274c5878cbac3d790958fe00bcbd
Dannishvir <89144023+Dannishvir@users.noreply.github.com>
2021-08-18 19:13:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite(200,200,50,50)
+}
+
+function draw() 
+{
+  background(30);
+
+  if (keyDown("right")){
+    box.x = box.x + 4
+    
+  }
+  if (keyDown("left")){
+    box.x = box.x - 4 
+  }
+  if (keyDown("up")){
+    box.y = box.y - 4
+  }
+
+  if (keyDown("down")){
+    box.y = box.y + 4
+  }
+  drawSprites()
+}
+
+
+
+
